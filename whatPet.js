ef889267b7e1293ba2a538da710dfe8f4e9df813
griselleong <griselleong@gmail.com>
2020-10-16 18:48:22 -0400

    Create whatPet.js

---
+++ whatPet.js
@@ -0,0 +1,121 @@
+let groceryItems = [
+  { 
+    item: 'bagels', 
+    price: 8.27 
+  },
+  { 
+    item: 'water chestnuts', 
+    price: 3.96 
+  },
+  { 
+    item: 'apricots', 
+    price: 7.44 
+  },
+  { 
+    item: 'cooking wine', 
+    price: 14.22 
+  },
+  { 
+    item: 'powdered sugar', 
+    price: 12.7 
+  },
+  { 
+    item: 'dates', 
+    price: 6.59 
+  },
+  { 
+    item: 'papaya', 
+    price: 8.75 
+  },
+  { 
+    item: 'hash browns', 
+    price: 0.63 
+  },
+  { 
+    item: 'mussels', 
+    price: 11.75 
+  },
+  { 
+    item: 'eggplant parmesan', 
+    price: 7.06 
+  },
+  { 
+    item: 'cornmeal', 
+    price: 12.76 
+  },
+  { 
+    item: 'curry leaves', 
+    price: 1.56 
+  },
+  { 
+    item: 'kiwi', 
+    price: 17.88 
+  },
+  { 
+    item: 'coconut milk', 
+    price: 5.02 
+  },
+  { 
+    item: 'nutmeg', 
+    price: 12.00 
+  },
+  { 
+    item: 'blackberries', 
+    price: 17.48 
+  },
+  { 
+    item: 'provolone', 
+    price: 12.21 
+  },
+  { 
+    item: 'tomato sauce', 
+    price: 7.21 
+  },
+  { 
+    item: 'ice cream', 
+    price: 18.51 
+  },
+  { 
+    item: 'pesto', 
+    price: 10.65 
+  },
+  { 
+    item: 'remoulade', 
+    price: 13.72 
+  },
+  { 
+    item: 'ham', 
+    price: 15.49 
+  },
+  { 
+    item: 'cranberries', 
+    price: 10.00
+  },
+  { 
+    item: 'feta cheese', 
+    price: 8.2 
+  },
+  { 
+    item: 'sauerkraut', 
+    price: 5.94
+   }
+];
+
+/*
+Write the code to find the answer to the following 
+Then take the first letter of the grocery item that matches your answer
+
+GROUP A
+1) Cheapest Grocery Item
+2) Most Expensive Grocery Item
+3) Grocery Item with the Price that is Most Expensive minus Cheapest
+
+GROUP B
+1) Longest Grocery Item Name
+2) Grocery Item with the Average price of all the items
+
+GROUP C
+1) Grocery Item with the Median price of all the items
+2) Grocery Item with a price that is divisible by 3 
+
+*/
